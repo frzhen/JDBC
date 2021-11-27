039f0313b9e470c8483cde6bd48ef52c7cd60f8c
Fred Zhen <13911239075@163.com>
2021-11-27 23:06:53 +0800

    Added saveNewAuthor() implementation and testSaveAuthor, run successful

--- src/main/java/guru/ysy/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/guru/ysy/jdbc/dao/AuthorDaoImpl.java
@@ -33,11 +33,17 @@ public class AuthorDaoImpl implements AuthorDao {
 
     @Override
     public Author saveNewAuthor(Author author) {
-        return null;
+        jdbcTemplate.update("INSERT INTO author (first_name, last_name) VALUES (?,?)",
+                author.getFirstName(), author.getLastName());
+
+        Long createdId = jdbcTemplate.queryForObject("SELECT LAST_INSERT_ID()", Long.class);
+
+        return this.getById(createdId);
     }
 
     @Override
     public Author updateAuthor(Author author) {
+
         return null;
     }
 
